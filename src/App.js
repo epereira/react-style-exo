be7de6e891b8365ad399f38eb3d801e8c4a5d074
Emmanuel PEREIRA <epereira777@gmail.com>
2022-09-27 14:06:05 +0200

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { jsx, css } from '@emotion/react';
+import style from '@emotion/styled';
+
+
+const Card = style.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  max-width: 400px;
+  :hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+  & > img{
+    width:100%;
+  }
+`;
+
+const CardContainer = style.div`
+  padding: 6px 16px;
+`;
+
+const Strong = style.b`
+  color: blue;
+  :hover {
+    color: red;
+    cursor: zoom-in;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Card>
+        <img src="./img/img_avatar.png" alt="Avatar" />
+        <CardContainer>
+          <h4><Strong>John Doe</Strong></h4> 
+          <p>React Developer</p> 
+        </CardContainer>
+    </Card>
     </div>
   );
 }
